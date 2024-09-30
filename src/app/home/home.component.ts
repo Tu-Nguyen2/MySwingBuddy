e586f67b2d6772a06ed6f8e90109d90414637728
Tu-Nguyen2 <nguyenT7002@hotmail.com>
2024-09-30 10:26:30 -0400

    Adding routing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,26 @@
 import { Component } from '@angular/core';
+import { UserHomeComponent } from '../user-home/user-home.component';
+import { MySwingBuddyComponent } from '../my-swing-buddy/my-swing-buddy.component';
+import { RouterLink, RouterLinkActive, RouterOutlet, Routes } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  title = 'Home'
 }
+
+const routes: Routes = [
+  {path: 'UserHome', component: UserHomeComponent},
+  {path: 'MySwingBuddy', component: MySwingBuddyComponent},
+]
+
+imports: [
+  UserHomeComponent,
+  MySwingBuddyComponent
+]
