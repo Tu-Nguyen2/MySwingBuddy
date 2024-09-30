59828df7b0ad226c86f0d723818fa3f782c9999b
Tu-Nguyen2 <nguyenT7002@hotmail.com>
2024-09-30 11:11:00 -0400

    Deleting changes and implementing it to app.component instead

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { UserHomeComponent } from './user-home/user-home.component';
+import { MySwingBuddyComponent } from './my-swing-buddy/my-swing-buddy.component';
+import { RouterLink, RouterLinkActive, RouterOutlet, Routes } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MySwingBuddy';
+  title = 'Routing-App';
 }
+
+const routes: Routes = [
+  {path: 'UserHome', component: UserHomeComponent},
+  {path: 'MySwingBuddy', component: MySwingBuddyComponent},
+]
+imports: [
+  UserHomeComponent,
+  MySwingBuddyComponent
+]
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,27 +1,12 @@
-import { ApplicationConfig, Component } from '@angular/core';
-import { UserHomeComponent } from '../user-home/user-home.component';
-import { MySwingBuddyComponent } from '../my-swing-buddy/my-swing-buddy.component';
-import { RouterLink, RouterLinkActive, RouterOutlet, Routes } from '@angular/router';
-import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-my-swing-buddy',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-  title = 'Home'
+
 }
-
-
-const routes: Routes = [
-  {path: 'UserHome', component: UserHomeComponent},
-  {path: 'MySwingBuddy', component: MySwingBuddyComponent},
-]
-
-imports: [
-  UserHomeComponent,
-  MySwingBuddyComponent
-]
