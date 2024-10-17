9a1f71e9b47c358a07e31d8bfa34a4258f039eea
Tu-Nguyen2 <nguyenT7002@hotmail.com>
2024-10-17 15:37:29 -0400

    amplify again

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,10 @@ import { AppComponent } from './app/app.component';
 
 import Amplify from 'aws-amplify';
 import { aws_config } from 'aws-cdk-lib';
+import outputs from '../amplify_outputs.json';
+
+
+// Amplify.configure(outputs);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
