d7c374744977d1d6899b5beee00b2ba9c5fd9ae4
Paul Pilipczuk <paul.pilipczuk.20@cnu.edu>
2024-10-19 10:02:14 -0400

    Amplify should be configured now, main.ts updated, app.component.ts needs to be updated

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 
-import Amplify from 'aws-amplify';
-import { aws_config } from 'aws-cdk-lib';
-import outputs from '../amplify_outputs.json';
+import { Amplify } from 'aws-amplify';
+import config from './amplifyconfiguration.json';
 
+Amplify.configure(config);
 
-// Amplify.configure(outputs);
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
