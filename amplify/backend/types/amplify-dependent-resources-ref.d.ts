522f7d6a830e127dd25eecf57e6f3e89a504d027
Paul Pilipczuk <paul.pilipczuk.20@cnu.edu>
2024-10-22 09:41:47 -0400

    dependencies update

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,28 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "myswingbuddy32e01f08": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "S3Trigger67d34bd8": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
+    }
+  },
+  "storage": {
+    "uservideo": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
